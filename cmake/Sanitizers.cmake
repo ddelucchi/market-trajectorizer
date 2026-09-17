b9ecd0e69f0a8ba2f1d9a4bba9c6bab178495f36
Daxx Delucchi <84737356+ddelucchi@users.noreply.github.com>
2026-09-17 16:07:40 -0700

    build: make sanitizer flags compiler-aware

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,13 +1,31 @@
-option(MT_ENABLE_ASAN "Enable AddressSanitizer (host-only builds)" OFF)
-option(MT_ENABLE_UBSAN "Enable UndefinedBehaviorSanitizer" OFF)
+option(MT_ENABLE_ASAN "Enable AddressSanitizer for host C++ targets" OFF)
+option(MT_ENABLE_UBSAN "Enable UndefinedBehaviorSanitizer where supported" OFF)
 
 function(mt_apply_sanitizers tgt)
-  if(MT_ENABLE_ASAN)
-    target_compile_options(${tgt} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-fsanitize=address -fno-omit-frame-pointer>)
-    target_link_options(${tgt}    PRIVATE -fsanitize=address)
+  if(MSVC)
+    if(MT_ENABLE_ASAN)
+      target_compile_options(${tgt} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:/fsanitize=address>)
+      target_link_options(${tgt} PRIVATE /fsanitize=address)
+    endif()
+
+    if(MT_ENABLE_UBSAN)
+      message(WARNING "MT_ENABLE_UBSAN is not supported by MSVC and will be ignored for target ${tgt}")
+    endif()
+    return()
   endif()
+
+  if(MT_ENABLE_ASAN)
+    target_compile_options(
+      ${tgt}
+      PRIVATE
+        $<$<COMPILE_LANGUAGE:CXX>:-fsanitize=address>
+        $<$<COMPILE_LANGUAGE:CXX>:-fno-omit-frame-pointer>
+    )
+    target_link_options(${tgt} PRIVATE -fsanitize=address)
+  endif()
+
   if(MT_ENABLE_UBSAN)
     target_compile_options(${tgt} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-fsanitize=undefined>)
-    target_link_options(${tgt}    PRIVATE -fsanitize=undefined)
+    target_link_options(${tgt} PRIVATE -fsanitize=undefined)
   endif()
 endfunction()
